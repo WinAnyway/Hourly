82955397ce2faf6ea8d82384ca8d1fafada44a20
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-05-23 23:25:07 +0200

    polishing

--- src/main/java/pl/przygudzki/hourly/domain/commands/InvalidCommandException.java
+++ src/main/java/pl/przygudzki/hourly/domain/commands/InvalidCommandException.java
@@ -1,15 +1,15 @@
 package pl.przygudzki.hourly.domain.commands;
 
-public class InvalidCommandException extends RuntimeException{
+public class InvalidCommandException extends RuntimeException {
 
-    private Validatable.ValidationErrors errors;
+	private Validatable.ValidationErrors errors;
 
-    public InvalidCommandException(Validatable.ValidationErrors errors) {
+	public InvalidCommandException(Validatable.ValidationErrors errors) {
+		this.errors = errors;
+	}
 
-        this.errors = errors;
-    }
+	public Validatable.ValidationErrors getErrors() {
+		return errors;
+	}
 
-    public Validatable.ValidationErrors getErrors() {
-        return errors;
-    }
 }
